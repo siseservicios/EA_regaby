e9681a78c21172fb4e4de2d1d5e48a4bc4d06782
Gabriela Rivero <regaby@gmail.com>
2022-05-17 16:34:44 -0300

    [UPD] ajuste query y quito botones crear y editar de la vista

--- drb_report_management_debt/models/report_management_debt.py
+++ drb_report_management_debt/models/report_management_debt.py
@@ -51,7 +51,7 @@ class DrbReportManagementDebt(models.Model):
         currency_field='company_currency_id',
         readonly=True,
     )
-    
+
     balance_to_billed = fields.Monetary(
         string='Balance to Billed',
         currency_field='company_currency_id',
@@ -80,13 +80,13 @@ class DrbReportManagementDebt(models.Model):
         string='Date',
         readonly=True,
     )
-    
+
     @api.model_cr
     def init(self):
         tools.drop_view_if_exists(self._cr, 'drb_report_management_debt')
         self._cr.execute("""
             CREATE OR REPLACE VIEW drb_report_management_debt AS (
-                (SELECT so.id id,
+                (SELECT coalesce(so.partner_id,0)+coalesce(so.id, 0)+coalesce(ai.id, 0)+coalesce(ap.id,0) id,
                     ai.currency_id as company_currency_id,
                     so.partner_id as partner_id,
                     so.id as sale_order_id,
@@ -100,7 +100,7 @@ class DrbReportManagementDebt(models.Model):
                     ai.residual as residual,
  					ap.id as payment_id,
  					ap.amount as payment_amount,
-                    (so.amount_total - ap.amount) as total_payment_amount, 
+                    (so.amount_total - ap.amount) as total_payment_amount,
  					ap.payment_date as payment_date
                 FROM  sale_order as so
 				LEFT JOIN account_invoice as ai ON (ai.origin = so.name AND ai.state in ('open','paid') )
@@ -108,7 +108,7 @@ class DrbReportManagementDebt(models.Model):
 				left join account_payment ap on (inv_pay.payment_id=ap.id)
  				WHERE so.state in ('sale'))
                 UNION
-                (select ap.id + 50000,
+                (select coalesce(ap.partner_id) + coalesce(ap.id) + 50000,
                 ap.currency_id as company_currency_id,
                 ap.partner_id as partner_id,
                     null as sale_order_id,
@@ -121,7 +121,7 @@ class DrbReportManagementDebt(models.Model):
                     null as balance_to_billed,
                     null as  residual,
  					ap.id as payment_id,
- 					ap.amount as payment_amount, 
+ 					ap.amount as payment_amount,
  					null as total_payment_amount,
  					ap.payment_date as payment_date
                 from account_payment ap
